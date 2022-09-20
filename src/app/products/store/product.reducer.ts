d3c50566fc6521725b3a7393d20d079c827fb26f
Mathias Fritsch <Mathias.Fritsch@senacor.com>
2022-09-20 18:05:28 +0200

    ngrx enity in products

--- src/app/products/store/product.reducer.ts
+++ src/app/products/store/product.reducer.ts
@@ -3,16 +3,19 @@ import * as ProductActions from './product.actions';
 import { Product } from 'src/app/shared/product';
 import { state } from '@angular/animations';
 export const productFeatureKey = 'product';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 
-export interface State {
+export const adapter: EntityAdapter<Product> = createEntityAdapter<Product>({});
+
+export interface State extends EntityState<Product> {
   products: Product[];
   loading: boolean;
 }
 
-export const initialState: State = {
+export const initialState: State = adapter.getInitialState({
   products: [],
   loading: false,
-};
+});
 
 export const reducer = createReducer(
   initialState,
@@ -22,7 +25,7 @@ export const reducer = createReducer(
   }),
   on(ProductActions.loadProductsSuccess, (state, action) => {
     return {
-      ...state,
+      ...adapter.setAll(action.data, state),
       products: action.data,
       loading: false,
     };
@@ -31,3 +34,7 @@ export const reducer = createReducer(
     return { ...state, loading: false };
   })
 );
+const { selectIds, selectEntities, selectAll, selectTotal } =
+  adapter.getSelectors();
+export const selectAllProductsE = selectAll;
+export const selectProductsTotal = selectTotal;
